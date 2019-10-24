0694701070449ac335de08eada9d4bc95f7e91b6
TheuZero <45233044+TheuZero@users.noreply.github.com>
2019-10-24 00:18:57 -0300

    Padronizando menu
    ainda falta pegar as informações pertinentes de cada menu e arrumar o marcador de botão, provavelmente tirar ele de cada canvas e manter um só.

--- Assets/Bagulhos do Leandro/Assets/Script/Matheus/ControllerButtons.cs
+++ Assets/Bagulhos do Leandro/Assets/Script/Matheus/ControllerButtons.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ControllerButtons : MonoBehaviour
 {
-    public KeyCode leftInput = KeyCode.LeftArrow;
-    public KeyCode rightInput = KeyCode.RightArrow;
-    public KeyCode upInput = KeyCode.UpArrow;
-    public KeyCode downInput = KeyCode.DownArrow;
-    public KeyCode jumpInput = KeyCode.Space;
-    public KeyCode attackInput = KeyCode.Z;
-    public KeyCode defendInput;
-    public KeyCode specialInput = KeyCode.C;
-    public KeyCode techInput = KeyCode.X;
+    public KeyCode p1leftInput = KeyCode.LeftArrow;
+    public KeyCode p1rightInput = KeyCode.RightArrow;
+    public KeyCode p1upInput = KeyCode.UpArrow;
+    public KeyCode p1downInput = KeyCode.DownArrow;
+    public KeyCode p1jumpInput = KeyCode.Space;
+    public KeyCode p1attackInput = KeyCode.Z;
+    public KeyCode p1defendInput;
+    public KeyCode p1specialInput = KeyCode.C;
+    public KeyCode p1techInput = KeyCode.X;
+
+    public KeyCode p2leftInput = KeyCode.A;
+    public KeyCode p2rightInput = KeyCode.D;
+    public KeyCode p2upInput = KeyCode.W;
+    public KeyCode p2downInput = KeyCode.S;
+    public KeyCode p2jumpInput = KeyCode.F;
+    public KeyCode p2attackInput = KeyCode.E;
+    public KeyCode p2defendInput;
+    public KeyCode p2specialInput = KeyCode.G;
+    public KeyCode p2techInput = KeyCode.R;
     void Start()
     {
         
--- Assets/Bagulhos do Leandro/Assets/Script/Matheus/Menus.cs
+++ Assets/Bagulhos do Leandro/Assets/Script/Matheus/Menus.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 public class Menus : MonoBehaviour
 {
     MenuReferences menuReferences;
+    public ControllerButtons controller;
     public GameObject[] acessibleMenus;
     public Button[] actualButtons;
     float actualIndex;
     void OnEnable()
     {
+
         menuReferences = transform.parent.GetComponent<MenuReferences>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A)){
+            Debug.Log(actualButtons[0].name);
+        }
     }
 }
